b6a8f8c68cfb1b6c157a75b57272f49ae73f7b5e
Frankie Foston <frankie.foston@spire.com>
2021-02-18 18:04:36 +0000

    [errors] unified error messages

--- validatron/src/validators.rs
+++ validatron/src/validators.rs
@@ -35,7 +35,7 @@ where
     if *value == other {
         Ok(())
     } else {
-        Err(Error::new(format!("{} != {}", value, other)))
+        Err(Error::new(format!("'{}' must equal '{}'", value, other)))
     }
 }
 
@@ -52,7 +52,7 @@ where
     R: Display,
 {
     if *value < min {
-        Err(Error::new(format!("{} is less than {}", value, min)))
+        Err(Error::new(format!("'{}' must be greater than or equal to '{}'", value, min)))
     } else {
         Ok(())
     }
@@ -93,7 +93,7 @@ where
     R: Display,
 {
     if *value > max {
-        Err(Error::new(format!("'{}' is greater than '{}'", value, max)))
+        Err(Error::new(format!("'{}' must be less than or equal to '{}'", value, max)))
     } else {
         Ok(())
     }
